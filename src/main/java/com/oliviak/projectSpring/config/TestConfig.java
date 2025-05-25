7242684d1793e24b35fa9dfc7195139e0df21095
danieloliviak355 <danieloliviak355@penguin>
2025-05-25 17:54:56 +0100

    assoation manytomany with joinTable

--- src/main/java/com/oliviak/projectSpring/config/TestConfig.java
+++ src/main/java/com/oliviak/projectSpring/config/TestConfig.java
@@ -42,17 +42,22 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "daniel", "daniel@gmail.com", "999999769", "123456");
         User u2 = new User(null, "oliviak", "oliviak@gmail.com", "999999943", "123456");
         User u3 = new User(null, "pinto", "pinto@gmail.com", "999999129", "123456");
-        
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
         Order o1 = new Order(null, Instant.parse("2023-03-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2023-06-12T20:44:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2023-10-26T13:31:22Z"), OrderStatus.PAID, u3);
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet", 90.5, "");
         Product p2 = new Product(null, "Smart TV", "Lorem ipsum dolor sit amet", 2190.0, "");
-
-        userRepository.saveAll(Arrays.asList(u1, u2, u3));
-        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         category.saveAll(Arrays.asList(c1, c2, c3));
         productRepository.saveAll(Arrays.asList(p1, p2));
+
+        p1.getCategories().add(c2);
+        p2.getCategories().add(c1);
+        p2.getCategories().add(c3);
+        productRepository.saveAll(Arrays.asList(p1, p2));
+
     }
 }
--- src/main/java/com/oliviak/projectSpring/entities/Category.java
+++ src/main/java/com/oliviak/projectSpring/entities/Category.java
@@ -2,10 +2,13 @@ package com.oliviak.projectSpring.entities;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,7 +19,8 @@ public class Category implements java.io.Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new java.util.HashSet<>();
     
     public Category() {
